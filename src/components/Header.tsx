5e55278ab4d154aa26ee0605e42e8415f38c9a02
KEREM BAS <kerembas2000@gmail.com>
2024-10-10 10:45:50 +0300

    logo fixed
    fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box  } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Avatar } from '@mui/material';
 import { styled } from '@mui/system';
 import Link from 'next/link';
 
@@ -14,9 +14,14 @@ const Header: React.FC = () => {
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Box display="flex" alignItems="center">
           <Link href="/" passHref>
-          <Typography variant="h6" fontWeight="bold" sx={{ color: '#90caf9' }}>
-            JUSTCHAT
-          </Typography>
+            <Box display="flex" alignItems="center" >
+              <Box sx={{ paddingRight: 1 }}>
+                <Avatar alt="JUSTCHAT" src="/assets/app-icon.png"   sx={{ width: 35, height: 35 ,bgcolor:  '#90caf9'}}/>
+              </Box>
+              <Typography variant="h6" fontWeight="bold" sx={{ color: '#90caf9' }}>
+                JUSTCHAT
+              </Typography>
+            </Box>
           </Link>
         </Box>
       </Toolbar>
